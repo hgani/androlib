090dea74fd61b99e909fb973fe9607c4516d243c
Agus Syahputra <agus.putra99@gmail.com>
2017-04-19 15:12:50 +0700

    impl LauncherHelper#call and #mail

--- ganilib/src/main/java/com/gani/lib/screen/LauncherHelper.java
+++ ganilib/src/main/java/com/gani/lib/screen/LauncherHelper.java
@@ -1,8 +1,13 @@
 package com.gani.lib.screen;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 public class LauncherHelper {
   private Context context;
@@ -20,10 +25,28 @@ public class LauncherHelper {
   }
 
   public void call(String number) {
-    // TODO
+//    Intent i = new Intent(Intent.ACTION_CALL);
+//    i.setData(Uri.parse("tel:" + number));
+//    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+//      Toast.makeText(context, "Please grant the  permission to call", Toast.LENGTH_SHORT).show();
+//      ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.CALL_PHONE}, 1);
+//    } else {
+//      context.startActivity(i);
+//    }
+
+    Intent i = new Intent(Intent.ACTION_DIAL);
+    i.setData(Uri.parse("tel:" + number));
+    context.startActivity(i);
   }
 
   public void mail(String to, String subject, String message) {
-    // TODO
+    Intent i = new Intent(Intent.ACTION_SEND);
+    String[] s = {to};
+    i.putExtra(Intent.EXTRA_EMAIL, s);
+    i.putExtra(Intent.EXTRA_SUBJECT, subject);
+    i.putExtra(Intent.EXTRA_TEXT, message);
+    i.setType("message/rfc822");
+    Intent chooser = Intent.createChooser(i, "Launch Email");
+    context.startActivity(chooser);
   }
 }

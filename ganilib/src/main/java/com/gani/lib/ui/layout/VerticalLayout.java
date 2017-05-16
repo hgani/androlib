4ba4b76b8025de7323e6e35f66e8306f75fc0add
dylan <dylanjforster8@gmail.com>
2017-05-16 13:03:39 +1000

    abstract linear layout modified

--- ganilib/src/main/java/com/gani/lib/ui/layout/VerticalLayout.java
+++ ganilib/src/main/java/com/gani/lib/ui/layout/VerticalLayout.java
@@ -1,11 +1,22 @@
 package com.gani.lib.ui.layout;
 
 import android.content.Context;
+import android.util.AttributeSet;
+
+import com.gani.lib.ui.view.ViewHelper;
 
 public class VerticalLayout extends AbstractLinearLayout<VerticalLayout> {
   public VerticalLayout(Context context) {
     super(context);
+    init();
+  }
 
+  private void init() {
     setOrientation(VERTICAL);
+  }
+
+  public VerticalLayout(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
   }
 }

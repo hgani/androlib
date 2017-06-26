35c48545dcc2a15676dd40ecb7965d8989f7a8c9
Hendrik Gani <hendrik.gani@gmail.com>
2017-06-26 18:42:37 +1000

    improve toast

--- ganilib/src/main/java/com/gani/lib/ui/alert/ToastUtils.java
+++ ganilib/src/main/java/com/gani/lib/ui/alert/ToastUtils.java
@@ -1,6 +1,5 @@
 package com.gani.lib.ui.alert;
 
-import android.content.Context;
 import android.os.Handler;
 import android.view.Gravity;
 import android.widget.Toast;
@@ -8,43 +7,17 @@ import android.widget.Toast;
 import com.gani.lib.ui.Ui;
 
 public class ToastUtils {
-//  // TODO: Remove. If you can pass a context, might as well use a snack bar
-//  public static void showToastInCenter(Context context, int resId) {
-//    Toast toast = Toast.makeText(context, resId, Toast.LENGTH_SHORT);
-//    toast.setGravity(Gravity.TOP, 0, context.getResources().getDisplayMetrics().heightPixels / 2);
-//    toast.show();
-//  }
-//
-//  // TODO: Remove. If you can pass a context, might as well use a snack bar
-//  public static void showNormal(RichContainer container, int resId) {
-//    showNormal(container, App.str(resId));
-//  }
-//
-//  // TODO: Remove. If you can pass a context, might as well use a snack bar
-//  public static void showNormal(RichContainer container, final String str) {
-//    final RichActivity activity = container.getGActivity();
-//    if (activity != null) {
-//      activity.runOnUiThread(new Runnable() {
-//        @Override
-//        public void run() {
-//          Toast.makeText(activity, str, Toast.LENGTH_SHORT).show();
-//        }
-//      });
-//    }
-//  }
-//
-//  // TODO: Remove. If you can pass a context, might as well use a snack bar
-//  public static void showGenericSuccess(RichContainer container) {
-//    showNormal(container, R.string.dialog_success);
-//  }
+  public static void showNormal(final String str) {
+    showNormal(str, Toast.LENGTH_SHORT);
+  }
 
   // Can be run from a background thread, e.g. HttpCallback.Rest.onRestSuccess()
-  public static void showNormal(final String str) {
+  public static void showNormal(final String str, final int duration) {
     Handler mainHandler = new Handler(Ui.context().getMainLooper());
     Runnable myRunnable = new Runnable() {
       @Override
       public void run() {
-        Toast.makeText(Ui.context(), str, Toast.LENGTH_SHORT).show();
+        Toast.makeText(Ui.context(), str, duration).show();
       }
     };
     mainHandler.post(myRunnable);
@@ -53,10 +26,6 @@ public class ToastUtils {
   public static void showNormal(int strId) {
     showNormal(Ui.str(strId));
   }
-
-//  public static void showGenericSuccess() {
-//    showNormal(R.string.dialog_success);
-//  }
 
   public static void showToastInCenter(int resId) {
     Toast toast = Toast.makeText(Ui.context(), resId, Toast.LENGTH_SHORT);

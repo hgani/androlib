3590fec6a15c4f96c1badcf910f85bab57a308dc
Hendrik Gani <hendrik.gani@gmail.com>
2017-08-23 16:19:10 +1000

    More robust parallel http

--- ganilib/src/main/java/com/gani/lib/utils/LocationManager.java
+++ ganilib/src/main/java/com/gani/lib/utils/LocationManager.java
@@ -1,7 +1,6 @@
 package com.gani.lib.utils;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -15,9 +14,6 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-
-import static android.R.attr.permission;
-import static java.security.AccessController.getContext;
 
 public class LocationManager {
   private static final String PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -36,30 +32,12 @@ public class LocationManager {
   };
 
   private Location location;
-//  private LocationFetchingListener fetchListener;
 
   private static final LocationManager INSTANCE = new LocationManager();
 
   public static LocationManager instance() {
     return INSTANCE;
   }
-
-//  public void updateLocationSilently(GActivity activity) {
-//    activity.fetchLastLocation(Manifest.permission.ACCESS_FINE_LOCATION, new GActivity.LocationFetchingListener() {
-//      @Override
-//      public void onSuccess(Location location) {
-////        GLog.t(getClass(), "Updated location: " + location.getLatitude() + ". " + location.getLongitude());
-//        LocationManager.this.location = location;
-//      }
-//
-//      @Override
-//      public void onFailure() {
-//        // Nothing to do
-//      }
-//    });
-//
-//
-//  }
 
   public void updateLocation(GActivity activity) {
     if (!updateLocationSilently(activity)) {
@@ -76,10 +54,6 @@ public class LocationManager {
     }
     return false;
   }
-//
-//  public static boolean updateLocationSilently(GActivity activity) {
-//    return activity.fetchLastLocationSilently();
-//  }
 
   public boolean isAccessPermitted() {
     return ContextCompat.checkSelfPermission(Ui.context(), PERMISSION) == PackageManager.PERMISSION_GRANTED;
@@ -113,42 +87,6 @@ public class LocationManager {
             FETCH_LISTENER.onFailure();
           }
         });
-
-
-
-//    GLog.t(getClass(), "fetchFusedLocation3");
-//    final LocationFetchingListener listener = fetchListener;
-//    if (listener != null) {  // Extra protection in case Android calls onRequestPermissionsResult() multiple times.
-//
-//      GLog.t(getClass(), "fetchFusedLocation4");
-//      this.locationFetchListener = null;
-//
-//      FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(getContext());
-//
-//      client.getLastLocation()
-//          .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-//            @Override
-//            public void onSuccess(Location location) {
-//              GLog.t(getClass(), "fetchFusedLocation5");
-//              // Got last known location. In some rare situations this can be null.
-//              if (location != null) {
-//                GLog.t(getClass(), "fetchFusedLocation6");
-//                listener.onSuccess(location);
-//              }
-//              else {
-//                GLog.t(getClass(), "fetchFusedLocation7");
-//                listener.onFailure();
-//              }
-//            }
-//          })
-//          .addOnFailureListener(this, new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//              GLog.t(getClass(), "fetchFusedLocation8");
-//              listener.onFailure();
-//            }
-//          });
-//    }
   }
 
 
